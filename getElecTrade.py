4e780afb3b9226ff5eaa7911d148a63bfb867789
yamamoto kenta <yamamoto7.pgming@gmail.com>
2020-03-25 14:23:58 +0900

    init

---
+++ getElecTrade.py
@@ -0,0 +1,36 @@
+# coding: UTF-8
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+# Chromeのpath
+options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+# headlessモード
+options.add_argument('--headless')
+# ChromeのWebDriverオブジェクトを作成する。
+driver = webdriver.Chrome(chrome_options=options)
+
+# URLからページ取得
+url = "http://www.jepx.org/market/index.html"
+driver.get(url)
+
+# レンダリングのための時間
+time.sleep(2)
+
+# レンダリング結果をPhantomJSから取得
+html = driver.page_source
+
+# BeautifulSoupでレンダリング済みのhtmlを扱う
+soup = BeautifulSoup(html, "html.parser")
+
+# 見たいやつ見つけて出力
+print ("DA-24 : " + soup.find(id="chartDAAll").string)
+print ("DA-DT : " + soup.find(id="chartDADay").string)
+print ("DA-PT : " + soup.find(id="chartDAPeak").string)
+print ("TTV   : " + soup.find(id="chartsumVolume").string)
+
+# 終了
+driver.quit()
